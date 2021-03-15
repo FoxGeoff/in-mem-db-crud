e3546e45e8369e5ea8f67fb0312e7e26e7efc4da
Geoff Fox <fox.geoffrey@gmail.com>
2021-03-15 15:55:10 -0700

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { VillainInMemDataService } from './services/villain-in-mem-data.service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // switch that ensures the in-memory-web-api will be used in non-production environments.
+    HttpClientModule,
+    environment.production ?
+      [] : InMemoryWebApiModule.forRoot(VillainInMemDataService, {delay: 1000})
   ],
   providers: [],
   bootstrap: [AppComponent]
